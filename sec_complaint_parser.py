aeb4d09f7f3c4ae9449270baff33666f322b1fda
Siddhartha (Sid) Somani <sidsomani2005@gmail.com>
2025-07-16 14:44:22 -0400

    formatting

--- sec_complaint_parser.py
+++ sec_complaint_parser.py
@@ -18,9 +18,7 @@ class SECComplaintParser():
         returns:
             pdf_text (str): text of pdf at filepath location
         """
-
         reader = PdfReader(pdf_filepath)
-
         return " ".join([page.extract_text() for page in reader.pages])
 
     @staticmethod
@@ -38,7 +36,6 @@ class SECComplaintParser():
         MIN_LENGTH_THRESHOLD = 300
 
         if len(pdf_text) < MIN_LENGTH_THRESHOLD:
-
             return True
         
         return False
@@ -47,7 +44,6 @@ class SECComplaintParser():
     def get_all_pdf_texts(pdf_directory: str, save_text: bool = False, save_directory: str = None) -> dict[str: str]:
         """
         Takes in a directory name, reads in text of all pdfs in directory
-
         args:
             pdf_directory (str): path to directory to read in
 
@@ -58,20 +54,16 @@ class SECComplaintParser():
         directory_texts = {}
         
         for filename in tqdm(os.listdir(pdf_directory)):
-
             pdf_text = SECComplaintParser.get_pdf_text(os.path.join(pdf_directory, filename)).lower()
 
             #if there is something wrong with the pdf, do not include it in the dataset
-            if check_faulty_pdf(pdf_text):
-
+            if SECComplaintParser.check_faulty_pdf(pdf_text):
                 continue
 
             directory_texts[filename.split(".")[0]] = pdf_text
 
             if save_text:
-
                 if save_directory is None:
-
                     raise RuntimeError("Need to specify a save directory")
 
                 with open(os.path.join(save_directory, filename.split(".")[0] + ".txt"), "w", encoding="utf-8") as save_file:
@@ -95,9 +87,7 @@ class SECComplaintParser():
         directory_texts = {}
 
         for filename in tqdm(os.listdir(directory_path)):
-
             with open(os.path.join(directory_path, filename), encoding="utf-8") as txt_file:
-
                 directory_texts[filename.split(".")[0]] = txt_file.read()
 
         return directory_texts
@@ -116,9 +106,7 @@ class SECComplaintParser():
         """
 
         for pattern_string in pattern_strings:
-
             if re.fullmatch(pattern_string, string_to_check) is not None:
-
                 return string_to_check
 
         return None
@@ -128,9 +116,7 @@ class SECComplaintParser():
         '''
         Clean the text by removing all characters except alphanumeric
         '''
-
         cleaned_string = re.sub(r'[^a-zA-Z0-9]', '', string_to_clean)
-
         return cleaned_string
     
     @staticmethod
@@ -144,43 +130,30 @@ class SECComplaintParser():
         returns:
             dict(str: str): dict matching section header to section
         """
-
         #first split text into lines
         text_lines = text.split("\n")
-
         sections = {}
-
         curr_section_header = "DOC_START"
         curr_section = []
 
         for line in text_lines:
-
             cleaned_line = SECComplaintParser.clean_string(line)
-            
             line_matched = SECComplaintParser.regex_fullmatch_any(cleaned_line, headings)
-
+            
             if line_matched is None:
-
                 curr_section.append(line)
-
             else:
-
                 sections[curr_section_header] = curr_section
-
                 curr_section_header = line_matched
                 curr_section = []
 
         sections[curr_section_header] = curr_section
-
         return sections
 
     @staticmethod
     def section_all_texts(texts: dict) -> dict:
-
         sectioned_texts = {}
-
         for text_key in texts:
-
             sectioned_texts[text_key] = SECComplaintParser.section_text(texts[text_key])
 
         return sectioned_texts
@@ -195,23 +168,17 @@ class SECComplaintParser():
         returns:
             list[str]: list of violated regulations
         """
-
         cfr_headers = ["claims for relief", "claims for action", "claim for relief", "claim for action", "cause of action"]
-
         filings_matched = 0
 
         for filename in filing_texts:
-
             match = False
-            
+
             for cfr_header in cfr_headers:
-
                 if cfr_header in filing_texts[filename]:
-
                     match = True
 
             if match:
-
                 filings_matched += 1
 
         return filings_matched
